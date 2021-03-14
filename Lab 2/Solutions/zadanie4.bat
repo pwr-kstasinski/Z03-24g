211bde02ebdd06901e6b6b04f2a333dd1ca1cf6f
Hyunnim-aid <54777130+Hyunnim-aid@users.noreply.github.com>
2021-03-14 22:59:15 +0100

    zad 1-4

---
+++ Lab 2/Solutions/zadanie4.bat
@@ -0,0 +1,25 @@
+@echo off
+
+
+setlocal enableextensions enabledelayedexpansion
+
+set n=15
+set a=1
+set b=1
+
+set s= %a% %b%
+
+:loop
+	if !n! equ 0 goto print
+	set /a n-=1
+	set /a c=a+b
+	set s=!s! !c!
+	set /a a=b
+	set /a b=c
+	goto :loop
+
+:print
+	echo !s!
+	echo.
+
+pause
