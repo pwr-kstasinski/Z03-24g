cb210c96c963be3beeae7bebd30789334404570e
MrTitainin <jmmrt@outlook.com>
2021-03-15 12:27:54 +0100

    Solution for ex4 improved

--- Lab 2/Solutions/4.bat
+++ Lab 2/Solutions/4.bat
@@ -1,11 +1,10 @@
 @echo off
+setlocal enabledelayedexpansion
 set /a P=0
 set /a C=1
-for /l %%x in (1, 1, %1) do call :Loop
-goto End
-:Loop
-	echo %C%
-	set /a N=%P%+%C%
-	set /a P=%C%
-	set /a C=%N%
-:End
+for /l %%x in (1, 1, %1) do (
+	echo !C!
+	set /a N=!P!+!C!
+	set /a P=!C!
+	set /a C=!N!
+)
