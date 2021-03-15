736b71d0ea08f84b1f18a359074c06e806e5f27e
Michal Mazur <254492@student.pwr.edu.pl>
2021-03-15 01:08:04 +0100

    added message when given invalid input

--- Lab 2/Solutions/254492/zadanie4.bat
+++ Lab 2/Solutions/254492/zadanie4.bat
@@ -1,10 +1,13 @@
 @echo off
 @REM W tym zadaniu zakladam, ze zerowy wyraz ciagu fibonacciego to 0, pierwszy to 1, itd.
 
+if %1% LSS 0 (
+    echo Error - given number has to be positive or 0
+    goto :eof
 
+)
 SET /A a = 0
 SET /A b = 1
-
 
 
 FOR /l %%i IN (0, 1, %1) DO call :ForBody
--- Lab 2/Solutions/254492/zadanie7.bat
+++ Lab 2/Solutions/254492/zadanie7.bat
@@ -1,5 +1,11 @@
 @echo off
 
+if %1% LSS 0 (
+    echo Error - given number has to be positive or 0
+    goto :eof
+
+)
+
 set /A result = 1
 
 FOR /l %%i IN (%1, -1, 2) DO call :ForBody %%i
