f10bc5c9a2e2e5343b12ed7b583cebd7917f060b
Hyunnim-aid <54777130+Hyunnim-aid@users.noreply.github.com>
2021-03-22 19:56:27 +0100

    sasdasdsda

--- LAB3/Zadanie4.py
+++ LAB3/Zadanie4.py
@@ -17,7 +17,7 @@ if(run):
 
         while choice > 3 or choice < 1: 
             choice = int(input("Wpisz poprawna wartosc (1, 2 lub 3): ")) 
-
+print()
         if choice == 1: 
             choice_name = 'Kamien'
         elif choice == 2: 
