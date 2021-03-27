830bca360419eb5a171d31047d7c0bc056fcce3f
Adamozo <adam.naworski2000@gmail.com>
2021-03-27 17:21:44 +0100

    added postfix to tree conversion and printLevelOrder

--- Lab 4/Solutions/list4.py
+++ Lab 4/Solutions/list4.py
@@ -1,9 +1,14 @@
 #infix to postfix conversion form https://cppsecrets.com/users/2582658986657266505064717765737646677977/INFIX-TO-POSTFIX-CONVERSION-USING-STACK.php
 
 
-OPERATORS = set(['+', '-', '*', '/', '(', ')', '^'])
-PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+OPERATORS = set(['+', '-', '*', '/', '(', ')'])
+PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2}
 
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
 
 def infix_to_postfix(expression):
 
@@ -42,9 +47,50 @@ def infix_to_postfix(expression):
 
 
 def postfix_to_tree(postfix):
-    print("")
-    
+    stack = []
 
+    for ch in postfix:
+        if ch not in OPERATORS:
+          stack.append(Node(ch))
+
+        else:
+            print()
+            temp = Node(ch)
+            temp.right = stack.pop()
+            temp.left = stack.pop()
+            stack.append(temp)
+
+    return  stack.pop()
+
+
+def printLevelOrder(root):
+    h = height(root)
+
+    for i in range(h):
+        helperPrintLevelOrder(root, i+1)
+
+
+def helperPrintLevelOrder(root, level):
+    if root is None:
+        return
+    if level == 1:
+        print(root.data, end=" ")
+    elif level > 1:
+        helperPrintLevelOrder(root.left, level - 1)
+        helperPrintLevelOrder(root.right, level - 1)
+
+
+def height(node):
+    if node is None:
+        return 0
+    else:
+        lh = height(node.left)
+        rh = height(node.right)
+
+        if lh > rh:
+            return lh + 1
+        else:
+            return rh + 1
 
 
 def work():
@@ -54,7 +100,9 @@ def work():
     print(exp)
     if len(exp) > 1:
         print("supa")
-        print(infix_to_postfix(exp))
+        temp = infix_to_postfix(exp)
+        print(temp)
+        printLevelOrder(postfix_to_tree(temp))
 
     else:
         print("next time do what I say")
