e38624f0dd13dea59a2ae6070987bff20a3eccd3
Hyunnim-aid <54777130+Hyunnim-aid@users.noreply.github.com>
2021-03-29 16:55:45 +0200

    LAB 4 - w pyte, ale bez drzewa

--- LAB4/zadanie1.py
+++ LAB4/zadanie1.py
@@ -1,4 +1,3 @@
-#while do poprawkii
 def checkCorrectInput(equation):
     correctInput = (' ', '+', '-', '/', '*', '.', '(', ')', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
     correct = True
@@ -14,28 +13,13 @@ def doYourJob(equation):
     except: 
         print("Cos poszlo nie tak o.O\nWpisz to wyrazenie normalnie...")
         main()
-
-def messageForInvalidCharacter():
-    print("\nNiepoprawnie wprowadzone znaki. Wez sie zastanow, a nie w testera sie bawisz.")
-
+    
 def main():
-    run = True
-
-    while run:
         equation = input("\nZdefiniuj swoje rownanie: ")
 
         if checkCorrectInput(equation):
             doYourJob(equation)
-            answer = input("\nCzy chcesz zakonczyc dzialanie programu? [Y/N]: ")
-            if answer == "Y" or answer == "y":
-                run = False
-                break
-            elif answer == "N" or answer == "n":
-                main()
-            else:
-                print("Niepoprawna odpowiedz... ale zalozmy, ze chcesz zakonczyc program.")
-                run = False
         else:
-            messageForInvalidCharacter()
+            print("\nNiepoprawnie wprowadzone znaki. Wez sie zastanow, a nie w testera sie bawisz.")
 
 main()
