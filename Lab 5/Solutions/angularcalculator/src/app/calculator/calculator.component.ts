ae14baa576a42200651f9ce6ce0ccaec8b827273
Łukasz Szczęśniak <kontakt@lukaszszczesniak.com>
2021-04-12 16:19:44 +0200

    Add basic buttons and listeners

--- Lab 5/Solutions/angularcalculator/src/app/calculator/calculator.component.ts
+++ Lab 5/Solutions/angularcalculator/src/app/calculator/calculator.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  display = '';
+  output = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  digit(num: number): void {
+    this.display += num.toString();
+  }
+
+  comma(): void {
+    this.display += '.';
+  }
+
+  bracket(opening: boolean): void {
+    if (opening)
+    {
+      this.display += '(';
+    }
+    else
+    {
+      this.display += ')';
+    }
+  }
+
+  infix_operator(operator: string): void {
+    this.display += operator;
+  }
+
+  calculate(): void {
+    this.output = 'Calculating!';
+  }
 }
