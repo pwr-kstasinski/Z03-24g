f218a9553136961d36e525adad14c287212b009e
Paweł Bytner <54777130+pawel-bytner@users.noreply.github.com>
2021-05-10 15:30:27 +0200

    Delete LAB3 directory

--- LAB3/Zadanie1.py
+++
@@ -1,7 +0,0 @@
-import glob, os
-print("Podaj sciezke: ")
-path = input()
-os.listdir(path)
-for file in glob.glob("*.txt"):
-    print(file)
-#D:\Politechnika-Wrocławska\Laboratoria\JezykiSkryptowe\Lista3\PrototypyLubTesty
