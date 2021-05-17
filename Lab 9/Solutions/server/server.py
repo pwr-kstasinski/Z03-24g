62ea9b8b1b67d2828a6db39274e86d6e3359b79e
MrTitainin <jmmrt@outlook.com>
2021-05-17 17:05:15 +0200

    Not working solution

--- Lab 9/Solutions/server/server.py
+++ Lab 9/Solutions/server/server.py
@@ -1,12 +1,36 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,render_template
 from werkzeug.http import HTTP_STATUS_CODES
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 ser = Flask(__name__)
-messages = []
-users = []
+ser.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///mudb.sqlite3"
+db = SQLAlchemy(ser)
+messageID = 0
 
-def message_from_json(data):
-    return Message(data['from'],data['to'],data['message'])
+def getNextMessageID():
+    messageID+=1
+    return messageID
+
+class messages(db.Model):
+    id = db.Column("id",db.Integer,primary_key=True)
+    fr = db.Column("from",db.String(30))
+    to = db.Column("to",db.String(30))
+    msg = db.Column("message",db.String(300))
+    def __init__(self,fr,to,msg):
+        self.id = getNextMessageID()
+        self.fr = fr
+        self.to = to
+        self.msg = msg
+        
+class users(db.Model):
+    login = db.Column("id",db.String(30),primary_key=True)
+    password = db.Column("password",db.String(30))
+    lastQuery = db.Column("lastQuery",db.DateTime)
+    def __init__(self,login,password):
+        self.login = login
+        self.password = password
+        self.lastQuery = datetime.datetime.now()
 
 def bad_request(message=None,status_code=400):
     payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
@@ -46,18 +70,19 @@ def download_messages():
                             type: "string"
                             example: "Hello there"
     """
-    global messages
     data = request.args
     if "id" not in data:
         return bad_request("Necessary parameter id not given")
     uid = data["id"]
-    if uid not in users:
+    user = users.query.filter_by(login=uid).first()
+    if not user:
         return bad_request("ID not registered")
-    result, rest = [], []
-    for x in messages:
-        (result, rest)[x["to"]!=uid].append(x)
-    messages = rest
-    return jsonify(result)
+    result = messages.query.filter_by(to=uid)
+    user.lastQuery = datetime.datetime.now()
+    json = jsonify(result)
+    result.delete()
+    db.session.commit()
+    return json
 
 @ser.route("/send",methods=["POST"])
 def accept_message():
@@ -90,11 +115,15 @@ def accept_message():
     print(data)
     if "from" not in data or "to" not in data or "message" not in data:
         return bad_request("Any of necessary parameters from/to/message not found")
-    if data["from"] not in users:
-        return bad_request("Sender's ID not registered")
-    if data["to"] not in users:
+    user = users.query.filter_by(login=data["to"]).first()
+    if not user:
         return bad_request("Receiver's ID not registered")
-    messages.append({"from":data["from"],"to":data["to"],"message":data["message"]})
+    user = users.query.filter_by(login=data["from"]).first()
+    if not user:
+        return bad_request("Sender's ID not registered")
+    user.lastQuery = datetime.datetime.now()
+    db.session.add(messages(data["from"],data["to"],data["message"]))###
+    db.session.commit()
     return "OK"
 
 @ser.route("/register",methods=["POST"])
@@ -116,9 +145,15 @@ def register_user():
     data = request.args
     if "id" not in data:
         return bad_request("Necessary parameter id not given")
-    if data["id"] in users:
+    if "pass" not in data:
+        return bad_request("Necessary parameter pass not given")
+    uid = data["id"]
+    user = users.query.filter_by(login=uid).first()
+    if user:
         return bad_request("ID already registered")
-    users.append(data["id"])
+    usr = users(data["id"],data["password"])
+    db.session.add(usr)
+    db.session.commit()
     return "OK"
 
 @ser.route("/login",methods=["GET"])
@@ -140,9 +175,33 @@ def login_user():
     data = request.args
     if "id" not in data:
         return bad_request("Necessary parameter id not given")
-    if data["id"] not in users:
-        return bad_request("ID not registered")
+    if "pass" not in data:
+        return bad_request("Necessary parameter pass not given")
+    uid = data["id"]
+    passw = data["pass"]
+    user = users.query.filter_by(login=uid,password=passw).first()
+    if not user:
+        return bad_request("ID or password wrong")
+    user.lastQuery = datetime.datetime.now()
+    db.session.commit()
     return "OK"
 
+@ser.route("/logged",methods=["GET"])
+def get_active_users():
+    """Endpoint for getting active users
+    ---
+    responses:
+        "200":
+            description: "Succesfully logged in"
+    """
+    timeTreshold = datetime.datetime.now() - datetime.timedelta(seconds=5)
+    ausrs = users.query.filter(users.lastQuery>timeTreshold)
+    return "OK"
+
+@ser.route("/api",methods=["GET"])
+def render_api():
+    return render_template("index.html")
+
 if __name__ == "__main__":
+    db.create_all()
     ser.run(debug=True, host="0.0.0.0")
--- Lab 9/Solutions/server/testserver.py
+++ Lab 9/Solutions/server/testserver.py
@@ -1,8 +1,8 @@
 import requests
 
-r = requests.post('http://127.0.0.1:5000/register',params={'id':'iandonlyi'})
+r = requests.post('http://127.0.0.1:5000/register',params={'id':'iandonlyi','pass':'ipass'})
 print(r)
-r = requests.post('http://127.0.0.1:5000/register',params={'id':'someofyou'})
+r = requests.post('http://127.0.0.1:5000/register',params={'id':'someofyou','pass':'ypass'})
 print(r)
 r = requests.post('http://127.0.0.1:5000/send',json={'from':'someofyou','to':'iandonlyi','message':'hello there'})
 print(r)
